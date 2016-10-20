88040fad83e8c3d17e87ed485dc18f425e6a6dcf
YaninaOlesya <yaninaolesya87@gmail.com>
2016-10-20 16:06:57 +0200

    добавлено несколько функций

--- sandbox/src/main/java/ru/java_testing/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/java_testing/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package ru.java_testing.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+        hello("world");
+        hello("user");
+        hello("Olesya");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4;
+        double b =6;
+        System.out.println("Площадь рямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody);
 	}
+
+	public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
