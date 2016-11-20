46604cfd8aba88fb016c6741c0b2a885fa2d9b37
YaninaOlesya <yaninaolesya87@gmail.com>
2016-11-20 18:40:06 +0700

    Сравнение списков групп в тесте удаления группы

--- addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java_testing.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
 
     }
 
--- addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java_testing.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by Олеся on 29.10.2016.
  */
@@ -12,16 +14,16 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isTheheAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test123", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
