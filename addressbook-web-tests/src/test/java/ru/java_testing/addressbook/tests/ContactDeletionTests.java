52624bc4497f66e325fee0f9e8e5e9e63c85091d
YaninaOlesya <yaninaolesya87@gmail.com>
2016-12-01 23:05:15 +0700

    Сравнение списков контактов в тесте удаления контакта

--- addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/ContactDeletionTests.java
@@ -1,7 +1,10 @@
 package ru.java_testing.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java_testing.addressbook.model.ContactData;
+
+import java.util.List;
 
 /**
  * Created by Олеся on 29.10.2016.
@@ -14,8 +17,14 @@ public class ContactDeletionTests extends TestBase{
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("test_name", "test_surname", "test_nickname", "test_title", "test_phone", "test_email", "test1"));
         }
-        app.getContactHelper().selectContact();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().returnToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 }
