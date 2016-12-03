d33ba8c2299d89182609f88bc54afad89cd2b6a1
YaninaOlesya <yaninaolesya87@gmail.com>
2016-12-03 16:48:03 +0700

    Заменила сравнение множест на сравнение упорядоченных списков групп

--- addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/GroupModificationTests.java
@@ -3,9 +3,7 @@ package ru.java_testing.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java_testing.addressbook.model.GroupData;
-
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -34,6 +32,6 @@ public class GroupModificationTests extends TestBase {
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
         after.sort(byId);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Assert.assertEquals(before, after);
     }
 }
