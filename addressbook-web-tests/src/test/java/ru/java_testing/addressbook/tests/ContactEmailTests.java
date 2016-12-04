5743526a87c7d56e6ee927bfb232f601b132a8cd
YaninaOlesya <yaninaolesya87@gmail.com>
2016-12-04 22:28:20 +0700

    Убрала withGroup("test1") при создании контакта

--- addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/ContactEmailTests.java
+++ addressbook-web-tests/src/test/java/ru/java_testing/addressbook/tests/ContactEmailTests.java
@@ -22,8 +22,7 @@ public class ContactEmailTests extends TestBase {
             app.contact().create(new ContactData().withFirstName("test_name").withLastName("test_surname")
                     .withNickname("test_nickname").withTitle("test_title").withAddress("test_address")
                     .withHomePhone("111").withMobilePhone("222").withWorkPhone("333")
-                    .withEmail1("email1").withEmail2("email2").withEmail3("email3")
-                    .withGroup("test1"));
+                    .withEmail1("email1").withEmail2("email2").withEmail3("email3"));
         }
     }
 
