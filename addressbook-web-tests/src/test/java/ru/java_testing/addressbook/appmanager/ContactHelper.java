2e43f66498d5f3ddfb77466bf669157cede2019e
YaninaOlesya <yaninaolesya87@gmail.com>
2016-12-04 19:11:06 +0700

    Тест для проверки адреса контакта (дополнение)

--- addressbook-web-tests/src/test/java/ru/java_testing/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/java_testing/addressbook/appmanager/ContactHelper.java
@@ -28,6 +28,7 @@ public class ContactHelper extends HelperBase{
         type(By.name("lastname"), contactData.getLastName());
         type(By.name("nickname"), contactData.getNickname());
         type(By.name("title"), contactData.getTitle());
+        type(By.name("address"), contactData.getAddress());
         type(By.name("home"), contactData.getHomePhone());
         type(By.name("mobile"), contactData.getMobilePhone());
         type(By.name("work"), contactData.getWorkPhone());
